cb9895024d56204c981aa0edd9989182c559f93b
Michiel Holtkamp <git@supermind.nl>
2021-12-19 21:05:18 +0100

    NEW: Multiple from addresses

--- conf/settings.example.py
+++ conf/settings.example.py
@@ -3,8 +3,8 @@ SMTP_DEBUG = 0
 
 SMTP_HOST = ''
 SMTP_PORT = 465
-SMTP_FROM = ''
-SMTP_TO = ''
+SMTP_FROM_ADDRESSES = ()
+SMTP_TO_ADDRESS = ''
 
 # these two can also be set by the environment variables with the same name
 SMTP_USERNAME = ''
--- send_health_probe.py
+++ send_health_probe.py
@@ -13,14 +13,19 @@ def enrich_settings_from_env(settings):
 
 def main():
     enrich_settings_from_env(settings)
-    msg = EmailMessage()
-    msg['Subject'] = 'Health check'
-    msg['From'] = settings.SMTP_FROM
-    msg['To'] = settings.SMTP_TO
-    msg.set_content('Health check email')
+
     with SMTP_SSL(host=settings.SMTP_HOST, port=settings.SMTP_PORT) as smtp:
+        msg = EmailMessage()
+        msg['Subject'] = 'Health check'
+        msg['To'] = settings.SMTP_TO_ADDRESS
+        msg.set_content('Health check email')
+
         smtp.set_debuglevel(settings.SMTP_DEBUG)
         smtp.login(settings.SMTP_USERNAME, settings.SMTP_PASSWORD)
-        smtp.send_message(msg)
+
+        for sender in settings.SMTP_FROM_ADDRESSES:
+            del msg['From']
+            msg['From'] = sender
+            smtp.send_message(msg)
 
 main()
