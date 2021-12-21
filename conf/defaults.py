485deb770de1a48faa24f258885dbd44f8f02084
Michiel Holtkamp <git@supermind.nl>
2021-12-21 22:57:12 +0100

    OPT: Added defaults.py
    Splitting settings up in defaults.py and
    settings.py (copied from settings.example.py)
    makes it a bit easier to add new settings
    without everyone having to update the settings.py.
    
    Unless they want to change the defaults of course,
    but that is still possible.

---
+++ conf/defaults.py
@@ -0,0 +1,16 @@
+# Set to 1 or 2 to show what we send and receive from the SMTP server
+SMTP_DEBUG = 0
+
+SMTP_HOST = ''
+SMTP_PORT = 465
+SMTP_FROM_ADDRESSES = ()
+SMTP_TO_ADDRESS = ''
+
+# these two can also be set by the environment variables with the same name
+SMTP_USERNAME = ''
+SMTP_PASSWORD = ''
+
+IMAP_HOSTNAME = ''
+IMAP_USERNAME = ''
+IMAP_PASSWORD = ''
+IMAP_LIST_FOLDER = 'INBOX'
--- conf/settings.example.py
+++ conf/settings.example.py
@@ -1,16 +1,16 @@
-# Set to 1 or 2 to show what we send and receive from the SMTP server
-SMTP_DEBUG = 0
+from .defaults import *
 
-SMTP_HOST = ''
-SMTP_PORT = 465
-SMTP_FROM_ADDRESSES = ()
-SMTP_TO_ADDRESS = ''
+# Override any settings here, like:
+# SMTP_HOST = ''
+# SMTP_FROM_ADDRESSES = ()
+# SMTP_TO_ADDRESS = ''
 
 # these two can also be set by the environment variables with the same name
-SMTP_USERNAME = ''
-SMTP_PASSWORD = ''
+# SMTP_USERNAME = ''
+# SMTP_PASSWORD = ''
 
-IMAP_HOSTNAME = ''
-IMAP_USERNAME = ''
-IMAP_PASSWORD = ''
-IMAP_LIST_FOLDER = 'INBOX'
+# IMAP_HOSTNAME = ''
+
+# these two can also be set by the environment variables with the same name
+# IMAP_USERNAME = ''
+# IMAP_PASSWORD = ''
